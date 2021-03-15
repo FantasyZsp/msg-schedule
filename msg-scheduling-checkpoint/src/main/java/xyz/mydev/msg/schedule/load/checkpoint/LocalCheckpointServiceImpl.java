76f87bbb77c36a0f202aa246303c142569590516
zhaosp-m1 <zhaoshipan@gmail.com>
2021-03-15 19:50:47 +0800

    save

--- msg-scheduling-checkpoint/src/main/java/xyz/mydev/msg/schedule/load/checkpoint/LocalCheckpointServiceImpl.java
+++ msg-scheduling-checkpoint/src/main/java/xyz/mydev/msg/schedule/load/checkpoint/LocalCheckpointServiceImpl.java
@@ -1,19 +1,36 @@
 package xyz.mydev.msg.schedule.load.checkpoint;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 
 /**
+ * 用于即时投递消息的补偿
+ *
  * @author zhaosp
  */
 public class LocalCheckpointServiceImpl implements CheckpointService {
+
+  public final LocalDateTime DEFAULT_CHECK_POINT = LocalDateTime.of(LocalDate.ofYearDay(2000, 1), LocalTime.MIN);
+
+  /**
+   * 默认从最开始的数据扫描
+   *
+   * @author ZSP
+   */
   @Override
   public LocalDateTime defaultStartCheckpoint(String targetTableName) {
-    return null;
+    return DEFAULT_CHECK_POINT;
   }
 
+  /**
+   * 当前检查点，对于即时消息，可以从15分钟前开始扫描
+   *
+   * @author ZSP
+   */
   @Override
   public LocalDateTime readCheckpoint(String targetTableName) {
     return null;
